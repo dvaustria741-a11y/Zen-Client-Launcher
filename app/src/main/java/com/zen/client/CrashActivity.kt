0fb2e5f119d6a889209383dd4f3f0aba5a85d103
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-21 20:21:21 +0800

    Move CrashActivity.kt to com.zen.client package

---
+++ app/src/main/java/com/zen/client/CrashActivity.kt
@@ -0,0 +1,30 @@
+package com.zen.client
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class CrashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crash)
+
+        findViewById<Button>(R.id.btnOpenCrashLogs).setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Crash Log")
+                .setMessage(CrashHandler.readLog(this))
+                .setPositiveButton("Close", null)
+                .show()
+        }
+
+        findViewById<Button>(R.id.btnCloseCrash).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            })
+            finish()
+        }
+    }
+}
